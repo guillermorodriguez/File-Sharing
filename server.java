4ca53735b4b72de07d2dfc1e9e63000356147e5b
gui_rod_dallas <gui_rod_dallas@hotmail.com>
2018-10-02 16:58:12 -0400

    Init

---
+++ server.java
@@ -0,0 +1,13 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server extends Thread {
+  
+  public static void main(String[] args){
+    System.out.println("Server Listening .... ");
+  }
+  
+}
