82d87ebd24b84655e7fb5bb0a87f65f3b4b56861
gui_rod_dallas@hotmail.com <FuzzyB0tt001>
2018-10-02 23:04:48 +0000

    client code

---
+++ client.java
@@ -0,0 +1,14 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class client extends Thread {
+  
+  public static void main(String[] args){
+    System.out.println("Server Listening .... ");
+  }
+  
+}
