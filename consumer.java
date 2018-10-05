77aca19a7c41d977dcd096c19fa10a3c5f0d2235
Guillermo Rodriguez <grodriguez@senegence.com>
2018-10-05 09:09:08 -0700

    Addendums

--- consumer.java
+++ consumer.java
@@ -8,7 +8,7 @@ import java.io.OutputStream;
 
 /**
  *	The purpose of this class is to function as a server instance that
- *	accepts data connections by the defined port from the command 
+ *	accepts data connections by the defined port from the command
  *	line and then processes data captured by client.
  */
 public class consumer extends Thread {
@@ -17,7 +17,7 @@ public class consumer extends Thread {
   protected Socket sock = null;
 
   private consumer(Socket sock){
-  	
+
   	System.out.println("Server Connection Established");
 
 	this.sock = sock;
@@ -26,13 +26,13 @@ public class consumer extends Thread {
   }
 
   public void run(){
-  
+
   	InputStream in = null;
 	OutputStream out = null;
 
 	try{
-	
-	
+
+
 	}
 	catch(Exception ex){
 		System.out.println("Exception: " + ex.getMessage());
@@ -45,38 +45,42 @@ public class consumer extends Thread {
    *	@param	args	command line argument parameters
    */
   public static void main(String[] args) {
-	
+
 	String expression = "\\d+";
 
-	if( args.length > 0 && args[0].matches(expression) ) {
+	if( args.length != 2 ) {
+    System.out.println("Fatal Exception. Invalid Input.");
+		System.out.println("First argument must be integer for port");
+    System.out.println("Second argument must be integer for max thread count");
+
+    System.exit(1);
+  }
+  else if( args[0].matches(expression) ) {
     		System.out.println("Server Listening on Port: " + args[0] );
-		
-		ServerSocket listener = null;
+        System.exit(1);
+  }
 
+  ServerSocket listener = null;
+
+	try{
+		listener = new ServerSocket(Integer.parseInt(args[0]));
+
+
+	}
+	catch(Exception ex){
+		System.out.println("Exception: " + ex.getMessage());
+	}
+	finally{
 		try{
-			listener = new ServerSocket(Integer.parseInt(args[0]));
-
-
+			if( listener != null ){ listener.close(); }
 		}
 		catch(Exception ex){
 			System.out.println("Exception: " + ex.getMessage());
 		}
-		finally{
-			try{
-				if( listener != null ){ listener.close(); }
-			}
-			catch(Exception ex){
-				System.out.println("Exception: " + ex.getMessage());
-			}
-		}
-
-		System.out.println("Server Shut Down");
-
 	}
-	else{
-		System.out.println("Fatal Exception. Invalid Input.");
-		System.out.println("First argument must be integer");
-	}
-  }
-  
+
+	System.out.println("Server Shut Down");
+
+
+
 }
