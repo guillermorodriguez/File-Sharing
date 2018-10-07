cc378b0b73fc6586f6869c83a363d9316fb9f3e1
Guillermo Rodriguez <grodriguez@senegence.com>
2018-10-07 14:24:44 -0700

    Addendums

--- consumer.java
+++ consumer.java
@@ -1,10 +1,14 @@
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.InputStream;
-import java.io.OutputStream;
+
+import java.util.*;
 
 /**
  *	The purpose of this class is to function as a server instance that
@@ -15,72 +19,107 @@ public class consumer extends Thread {
 
 
   protected Socket sock = null;
+  protected int max_threads = 0;
+  protected HashMap map = null;
 
-  private consumer(Socket sock){
+  private consumer(Socket sock, int max_threads){
+  	 System.out.println("Server Connection Established");
 
-  	System.out.println("Server Connection Established");
+	   this.sock = sock;
+     this.max_threads = max_threads;
 
-	this.sock = sock;
-
-	start();
+     this.map = new HashMap();
+	   start();
   }
 
   public void run(){
 
-  	InputStream in = null;
-	OutputStream out = null;
+  	InputStream input = null;
+	  OutputStream output = null;
+    BufferedReader reader = null;
 
-	try{
+  	try{
+      input = this.sock.getInputStream();
+      output = this.sock.getOutputStream();
+      reader = new BufferedReader(new InputStreamReader(input));
 
+      String data = "";
+      while( ( data = reader.readLine() ) != null ){
+        System.out.println("Input Stream: " + data);
 
-	}
-	catch(Exception ex){
-		System.out.println("Exception: " + ex.getMessage());
-	}
+        this.map.put(this.sock.getRemoteSocketAddress(), data);
+
+        output.write(("[Acknowledged]:"+data+'\n').getBytes());
+      }
+
+  	}
+  	catch(Exception ex){
+  		System.out.println("Exception: " + ex.getMessage());
+  	}
+    finally{
+      try{
+        input.close();
+        output.close();
+
+        sock.close();
+      }
+      catch(Exception ex){
+        System.out.println("Error With Object Scrubbing");
+      }
+    }
   }
 
-
   /**
-   *	Main entry point for server instance
+   *	Main entry point for consumer instance
    *	@param	args	command line argument parameters
    */
   public static void main(String[] args) {
 
-	String expression = "\\d+";
+  	String expression = "\\d+";
 
-	if( args.length != 2 ) {
-    System.out.println("Fatal Exception. Invalid Input.");
-		System.out.println("First argument must be integer for port");
-    System.out.println("Second argument must be integer for max thread count");
+  	if( args.length != 2 ) {
+      System.out.println("Fatal Exception. Invalid Input.");
+  		System.out.println("First argument must be integer for port");
+      System.out.println("Second argument must be integer for max thread count");
 
-    System.exit(1);
+      System.exit(1);
+    }
+    else if( !args[0].matches(expression) ){
+      System.out.println("Port Value Invalid");
+      System.exit(1);
+    }
+    else if( !args[1].matches(expression) ){
+      System.out.println("Thread Count Value Invalid");
+      System.exit(1);
+    }
+
+		System.out.println("Server Listening on Port: " + args[0] );
+    System.out.println("Thread Count Maximum: " + args[1]);
+
+
+    ServerSocket listener = null;
+
+  	try{
+  		listener = new ServerSocket(Integer.parseInt(args[0]));
+      while(true){
+        new consumer(listener.accept(), Integer.parseInt(args[1]));
+      }
+
+  	}
+  	catch(Exception ex){
+  		System.out.println("Exception: " + ex.getMessage());
+  	}
+  	finally{
+  		try{
+  			if( listener != null ){ listener.close(); }
+  		}
+  		catch(Exception ex){
+  			System.out.println("Exception: " + ex.getMessage());
+  		}
+  	}
+
+  	System.out.println("Server Shut Down");
+
   }
-  else if( args[0].matches(expression) ) {
-    		System.out.println("Server Listening on Port: " + args[0] );
-        System.exit(1);
-  }
-
-  ServerSocket listener = null;
-
-	try{
-		listener = new ServerSocket(Integer.parseInt(args[0]));
-
-
-	}
-	catch(Exception ex){
-		System.out.println("Exception: " + ex.getMessage());
-	}
-	finally{
-		try{
-			if( listener != null ){ listener.close(); }
-		}
-		catch(Exception ex){
-			System.out.println("Exception: " + ex.getMessage());
-		}
-	}
-
-	System.out.println("Server Shut Down");
-
-
 
 }
--- producer.java
+++ producer.java
@@ -1,14 +1,107 @@
 
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
+/**
+ *	The purpose of this class is to function as a client instance that
+ *	sends data by the defined port from the command line and then
+ *	accepts a response from the server.
+ */
 public class producer extends Thread {
-  
-  public static void main(String[] args){
-    System.out.println("Server Listening .... ");
+
+  protected String sink;
+  protected int port;
+
+  public producer(String sink, int port){
+    this.sink = sink;
+    this.port = port;
+
   }
-  
+
+  /**
+   *	Main entry point for producer instance
+   *	@param	args	command line argument parameters
+   */
+  public static void main(String[] args){
+    String SINK = "127.0.0.1";
+    Socket sock = null;
+    PrintWriter writer = null;
+    BufferedReader reader = null;
+    BufferedReader console = null;
+
+    System.out.println("Client Ready to Communicate .... ");
+    System.out.println("Print quit() to terminate.");
+
+    /*
+        Handle Input Validation
+    */
+    String expression_port = "\\d+";
+  	if( args.length != 2 ) {
+      System.out.println("Fatal Exception. Invalid Input.");
+  		System.out.println("First argument must be sink value");
+      System.out.println("Second argument must be port value");
+
+      System.exit(1);
+    }
+    else if( !args[0].equals(SINK) ){
+      System.out.println("Sink Value Invalid");
+      System.exit(1);
+    }
+    else if( !args[1].matches(expression_port) ){
+      System.out.println("Port Value Invalid");
+      System.exit(1);
+    }
+
+    /*
+        Set System Input / Output Configuration
+    */
+    try{
+      sock = new Socket(args[0], Integer.parseInt(args[1]));
+      writer = new PrintWriter(sock.getOutputStream(), true);
+      reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+      console = new BufferedReader(new InputStreamReader(System.in));
+    }
+    catch(UnknownHostException ex){
+      System.out.println("Unknown Host");
+      System.exit(1);
+    }
+    catch(IOException ex){
+      System.out.println("Input / Output Exception");
+      System.exit(1);
+    }
+
+    /*
+        Manage data streams
+    */
+    try{
+      while(true){
+          System.out.print("> ");
+          String input = console.readLine();
+          if( input.equals("quit()")) {
+            System.out.println("Terminated ...");
+            break;
+          }
+
+          writer.println(input);
+          System.out.println("Response: " + reader.readLine());
+      }
+
+      writer.close();
+      reader.close();
+      console.close();
+
+      sock.close();
+    }
+    catch(Exception ex){
+      System.out.println("Error Processing Client");
+    }
+  }
 }
